3073528ffeed50847b2e259418f6292ab0db09af
Rémi Roycourt <remiroycourt@gmail.com>
2019-05-23 23:45:37 +0200

    fix(landing-page): add normalize.css

--- pages/src/components/Layout/index.js
+++ pages/src/components/Layout/index.js
@@ -3,26 +3,12 @@ import PropTypes from "prop-types"
 
 import Header from "../Header"
 
+import "../../stylesheets/normalize.css"
 import "./style.css"
 
 const Layout = ({ children }) => (
   <div className="page">
     <Header />
-    {/* <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div> */}
     <main>{children}</main>
   </div>
 )
