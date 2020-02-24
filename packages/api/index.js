fe3b7b86111b642059607b5072c4de9f69e9d90a
kwiss <christophe@hooraylabs.com>
2020-02-24 22:55:43 +0100

    fix(api): remove graphql server

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,35 +1,16 @@
 import Koa from "koa";
 import cors from "@koa/cors";
 import bodyParser from "koa-bodyparser";
-import apolloServerKoa from "apollo-server-koa";
 import logger from "koa-pino-logger";
 
 import passport from "./config/passport";
 import authRoutes from "./routes/auth";
-import resolvers from "./graphql/resolvers";
-import typeDefs from "./graphql/schemas";
-
-import AuthAPI from "./graphql/datasources/auth";
-
-const API_PORT = process.env.API_PORT || 1337;
 
 const app = new Koa();
 const pino = logger({
   prettyPrint: true
 });
 app.use(pino);
-
-// The GraphQL datasources schema
-const dataSources = () => ({
-  authAPI: new AuthAPI()
-});
-
-// Apollo server
-const server = new apolloServerKoa.ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources
-});
 
 // body parser
 app.use(bodyParser());
@@ -40,19 +21,8 @@ const koaOptions = {
 };
 app.use(cors(koaOptions));
 
-// Graphql Middleware
-server.applyMiddleware({ app });
-
 // authentication
 app.use(passport.initialize());
 
 // routes
 app.use(authRoutes.routes());
-
-// server
-app.listen(API_PORT, () => {
-  pino.logger.info(`Server listening on port: ${API_PORT}`);
-  pino.logger.info(
-    `🚀 Graphql Server ready at http://localhost:${API_PORT}${server.graphqlPath}`
-  );
-});
