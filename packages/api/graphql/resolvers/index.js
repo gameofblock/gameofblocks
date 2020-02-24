601f2af28143741e5e3ffc2be1017c02d2b89d2d
kwiss <christophe@hooraylabs.com>
2020-02-24 22:45:42 +0100

    fix(files): remove useless files

--- packages/api/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import merge from "lodash/merge";
-import login from "./login";
-import signup from "./signup";
-
-const resolvers = merge(login, signup);
-
-export default resolvers;
--- packages/api/graphql/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import login from "./login";
-import signup from "./signup";
-import root from "./root";
-
-const typeDefs = [root, login, signup];
-
-export default typeDefs;
