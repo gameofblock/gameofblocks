de7851e46ee9433b28c43291aaf041ed91b0c26d
Rémi Roycourt <remiroycourt@gmail.com>
2020-02-25 01:30:57 +0100

    chore: typescript compilation

---
+++ packages/app/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <div className='container'>
+    <Head>
+      <title>Create Next App</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <main></main>
+    <footer></footer>
+  </div>
+);
+
+export default Home;
