c90623c7613498ded94e94e14ad86343cfeabec8
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-02-25 22:50:55 +0100

    Feat/init apollo (#22)
    * feat(apollo): set-up apollo client
    
    * refactor(lint): disable max-classes-per-file
    
    * fix(app): render component only one time

---
+++ packages/app/util/index.ts
@@ -0,0 +1,18 @@
+export const isBrowser = () => {
+  return typeof window !== 'undefined';
+};
+
+export const formatUserFromToken = currentUser => {
+  if (currentUser) {
+    return {
+      __typename: 'CurrentUser',
+      id: currentUser['https://hasura.io/jwt/claims']['x-hasura-user-id'],
+      role:
+        currentUser['https://hasura.io/jwt/claims']['x-hasura-default-role'],
+      serviceId:
+        currentUser['https://hasura.io/jwt/claims']['x-hasura-service-id']
+    };
+  } else {
+    return null;
+  }
+};
