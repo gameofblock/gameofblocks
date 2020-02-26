2a6565148883d660782fa6baa4c14f46b2b32047
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-02-26 13:33:54 +0100

    feat(auth): query current user (#26)

--- packages/app/components/user-context/index.tsx
+++ packages/app/components/user-context/index.tsx
@@ -1,30 +1,26 @@
 import { useQuery } from '@apollo/react-hooks';
 import React, { createContext, Fragment, FunctionComponent } from 'react';
 
-export const Context = createContext({});
+import { UserIdentifier } from './type';
+import { CURRENT_USER } from '../../queries/user';
 
-interface UserIdentifier {
-  role?: string;
-  userId?: string;
-}
+export const Context = createContext({});
 
 const UserProvider: FunctionComponent<UserIdentifier> = props => {
   const { role, userId, children } = props;
-  //   const { data } = useQuery(QUERY_TYPE[type], {
-  //     variables: {
-  //       userId: userId
-  //     }
-  //   });
+  const { data } = useQuery(CURRENT_USER, {
+    variables: {
+      userId
+    }
+  });
 
-  //   if (!data) {
-  //     return <Fragment>loading</Fragment>;
-  //   }
+  if (!data) {
+    return <Fragment>loading</Fragment>;
+  }
 
-  //   const [currentUser] = data.users;
+  const [currentUser] = data.users;
 
-  return (
-    <Context.Provider value={null /*currentUser*/}>{children}</Context.Provider>
-  );
+  return <Context.Provider value={currentUser}>{children}</Context.Provider>;
 };
 
 export const Provider: FunctionComponent<{
