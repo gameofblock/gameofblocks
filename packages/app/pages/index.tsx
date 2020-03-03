d24c98b7ca9a6a89c8b48a7aaf65127cb89c6104
kwiss <christophe@hooraylabs.com>
2020-03-03 18:56:10 +0100

    ci(gitlab): add test 2 for mirror

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const Home = () => (
   <div className='container'>
     <Head>
-      <title>Create Next App test</title>
+      <title>Create Next App test 2</title>
       <link rel='icon' href='/favicon.ico' />
     </Head>
     <main>hello</main>
