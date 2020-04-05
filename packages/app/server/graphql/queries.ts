457f60adedf56d27dbb1c4ec29c74344e59583a1
Rémi Roycourt <remiroycourt@gmail.com>
2020-04-05 20:37:37 +0200

    refactor(server): get user query

--- packages/app/server/graphql/queries.ts
+++ packages/app/server/graphql/queries.ts
@@ -2,10 +2,7 @@ import gql from 'graphql-tag';
 
 export const GET_USER = gql`
   query user($authId: String!) {
-    user(
-      limit: 1
-      where: { auth_id: { _eq: $authId }, active: { _eq: true } }
-    ) {
+    user(limit: 1, where: { auth_id: { _eq: $authId } }) {
       auth_id
       email
       picture
