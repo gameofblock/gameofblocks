a64909ca97575420c04dd9044b2290009e7a4b01
Rémi Roycourt <remiroycourt@gmail.com>
2020-04-11 02:41:20 +0200

    ci: new deployment step to upload public files to cdn

---
+++ .gitlab/ci/spaces-uploader.py
@@ -0,0 +1,28 @@
+# https://medium.com/@brianobilo/uploading-files-to-digital-ocean-spaces-using-api-keys-and-boto3-1f24242f6cd7
+
+import boto3
+from boto3 import session
+from botocore.client import Config
+from boto3.s3.transfer import S3Transfer
+
+#Use the API Keys you generated at Digital Ocean
+ACCESS_ID = 'xxxxxxxxxxxxxxxxxxx'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxx'
+
+
+# # Initiate session
+# session = session.Session()
+# client = session.client('s3', 
+#                         region_name='sfo2', 
+#                         endpoint_url='sfo2.digitaloceanspaces.com',
+#                         aws_access_key_id=ACCESS_ID,
+#                         aws_secret_access_key=SECRET_KEY)
+
+# transfer = S3Transfer(client)
+
+# # Uploads a file called 'name-of-file' to your Space called 'name-of-space'
+# # Creates a new-folder and the file's final name is defined as 'name-of-file' 
+# transfer.upload_file('name-of-file', 'name-of-space', 'new-folder'+"/"+'new-name-of-file')
+
+# #This makes the file you are have specifically uploaded public by default. 
+# response = client.put_object_acl(ACL='public-read', Bucket='name-of-space', Key="%s/%s" % ('new-folder', 'new-name-of-file'))
